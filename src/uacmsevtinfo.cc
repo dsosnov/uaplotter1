64dfc199926d31ef7eb6f4ae1ebe45b471852da5
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2017-08-03 14:28:55 +0200

    Added initialization for CMSMenuInfo and uacmsevtinfo files have been reformatted.

--- src/uacmsevtinfo.cc
+++ src/uacmsevtinfo.cc
@@ -11,25 +11,26 @@ ClassImp(uacmsevtinfo)
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-uacmsevtinfo::uacmsevtinfo(TChain * tree, 
-		  TDirectory * dir,    
-		  const bool cmstotem, 
-		  const short          int MC,  
-		  const short unsigned int Ncuts 
-		  ):uabase(cmstotem, MC, Ncuts, dir)
+uacmsevtinfo::uacmsevtinfo(TChain *tree,
+                           TDirectory *dir,
+                           const bool cmstotem,
+                           const short          int MC,
+                           const short unsigned int Ncuts
+                          ): uabase(cmstotem, MC, Ncuts, dir)
 {
   CMSevtInfo  = 0;
   CMStrigInfo = 0;
+  CMSmenuInfo = 0;
   CMSHLT      = 0;
-  if(tree_combined_flag){
-    tree->SetBranchAddress("cmsEvtUA",               &CMSevtInfo);  
-    tree->SetBranchAddress("cmsTrigUA",              &CMStrigInfo);  
-    tree->SetBranchAddress("cmsHLTTrigUA",           &CMSHLT);  
-  }else{
-    tree->SetBranchAddress("evtId",                  &CMSevtInfo);  
-    tree->SetBranchAddress("L1TrigRun2",             &CMStrigInfo);  
-    tree->SetBranchAddress("L1MenuRun2",             &CMSmenuInfo);  
-    tree->SetBranchAddress("HLTrig",                 &CMSHLT);  
+  if (tree_combined_flag) {
+    tree->SetBranchAddress("cmsEvtUA",               &CMSevtInfo);
+    tree->SetBranchAddress("cmsTrigUA",              &CMStrigInfo);
+    tree->SetBranchAddress("cmsHLTTrigUA",           &CMSHLT);
+  } else {
+    tree->SetBranchAddress("evtId",                  &CMSevtInfo);
+    tree->SetBranchAddress("L1TrigRun2",             &CMStrigInfo);
+    tree->SetBranchAddress("L1MenuRun2",             &CMSmenuInfo);
+    tree->SetBranchAddress("HLTrig",                 &CMSHLT);
   };
   create_histos();
 }
@@ -41,8 +42,8 @@ uacmsevtinfo::uacmsevtinfo(TChain * tree,
 
 uacmsevtinfo::~uacmsevtinfo()
 {
-  std::cout << "uacmsevtinfo::~uacmsevtinfo(): deleting " 
-	    << h1D->size() << "+" << h2D->size() << " histos" << std::endl;
+  std::cout << "uacmsevtinfo::~uacmsevtinfo(): deleting "
+            << h1D->size() << "+" << h2D->size() << " histos" << std::endl;
 }
 
 
@@ -50,20 +51,20 @@ uacmsevtinfo::~uacmsevtinfo()
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 void uacmsevtinfo::PrintEventInfo(const bool detailed)
-{ 
-  if(detailed){
+{
+  if (detailed) {
     std::cout << "uacmsevtinfo::PrintEventInfo: L1:\n\t";
-    for( short unsigned int b = 0; b < CMStrigInfo->triggerResults.size(); b++ )
-      if( CMStrigInfo->triggerResults.at( b ) )
+    for (short unsigned int b = 0; b < CMStrigInfo->triggerResults.size(); b++)
+      if (CMStrigInfo->triggerResults.at(b))
         std::cout << b << "   ";
     std::cout << std::endl;
-  }else{
+  } else {
     std::cout << "\nuacmsevtinfo::PrintEventInfo: L1 and tt of interest:\n\t";
-    for( short unsigned int b = 0; b < L1_TRIGGER_ARRAY.size(); b++ )
+    for (short unsigned int b = 0; b < L1_TRIGGER_ARRAY.size(); b++)
       std::cout << L1_TRIGGER_ARRAY[b] << "\t";
-    std::cout << std::endl;
-    for( short unsigned int b = 0; b < L1_TRIGGER_ARRAY.size(); b++ )
-      std::cout << CMStrigInfo->triggerResults.at( L1_TRIGGER_ARRAY.at( b ) ) << "\t";
+    std::cout << "\n\t";
+    for (short unsigned int b = 0; b < L1_TRIGGER_ARRAY.size(); b++)
+      std::cout << CMStrigInfo->triggerResults.at(L1_TRIGGER_ARRAY.at(b)) << "\t";
     std::cout << std::endl;
   }
 }
@@ -74,15 +75,15 @@ void uacmsevtinfo::PrintEventInfo(const bool detailed)
 
 bool uacmsevtinfo::FillLastEvent(const short unsigned int cut)
 {
-  if( cut>=n_cuts ){
-    std::cout << "uacmsevtinfo::FillLastEvent: required cut number is larger that possible, do nothing. Please define larger uaforward::n_cut!\n"; 
+  if (cut >= n_cuts) {
+    std::cout << "uacmsevtinfo::FillLastEvent: required cut number is larger that possible, do nothing. Please define larger uaforward::n_cut!\n";
     return false;
-  }; 
+  };
   run_vs_bx_h[cut]->Fill(CMSevtInfo->Bunch,    CMSevtInfo->Run);
   run_vs_ls_h[cut]->Fill(CMSevtInfo->LumiSect, CMSevtInfo->Run);
-  
-  for(unsigned int tb=0; tb<L1_TRIGGER_ARRAY.size(); tb++)
-    if(CMStrigInfo->triggerResults.at(L1_TRIGGER_ARRAY.at(tb)))
+
+  for (unsigned int tb = 0; tb < L1_TRIGGER_ARRAY.size(); tb++)
+    if (CMStrigInfo->triggerResults.at(L1_TRIGGER_ARRAY.at(tb)))
       triggers_h[cut]->Fill(tb);
 
   return true;
@@ -94,12 +95,12 @@ bool uacmsevtinfo::FillLastEvent(const short unsigned int cut)
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 bool uacmsevtinfo::ProceedEvent(const short unsigned int cut, const bool fill, const bool info)
 {
-  if( (!fill) ){
-    std::cout << "uacmsevtinfo::ProceedEvent: fill flag is false, that makes no sense, do nothing!\n"; 
+  if ((!fill)) {
+    std::cout << "uacmsevtinfo::ProceedEvent: fill flag is false, that makes no sense, do nothing!\n";
     return false;
-  }; 
+  };
   FillLastEvent(cut);
-  if(info) PrintEventInfo(true);
+  if (info) PrintEventInfo(true);
   return true;
 }
 
@@ -107,13 +108,13 @@ bool uacmsevtinfo::ProceedEvent(const short unsigned int cut, const bool fill, c
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-bool uacmsevtinfo::CheckHLT(const char* path)
+bool uacmsevtinfo::CheckHLT(const char *path)
 {
   bool on = false;
-  for(std::map<string,bool>::iterator hlt=CMSHLT->HLTmap.begin(); hlt!=CMSHLT->HLTmap.end(); ++hlt){
+  for (std::map<string, bool>::iterator hlt = CMSHLT->HLTmap.begin(); hlt != CMSHLT->HLTmap.end(); ++hlt) {
     //std::cout << (*hlt).first << "\t" << (*hlt).second << std::endl;
     std::size_t found = (*hlt).first.find(path);
-    if (found!=std::string::npos){
+    if (found != std::string::npos) {
       on =  !((*hlt).second);
     };
   };
@@ -134,30 +135,30 @@ void uacmsevtinfo::create_histos()
   run_vs_bx_h = new TH2F* [n_each_h2D];
   run_vs_ls_h = new TH2F* [n_each_h2D];
 
-  for(short unsigned int i=0;i<n_each_h1D; i++){
-    title1 = "triggers_h["; title1+=i; title1+="]";
-    title2 = title1; title2+="; trigger xxx";
-    triggers_h[i] = new TH1F(title1.Data(),title2.Data(), 12, 0,12); 
-    for(short unsigned int b=1; b<L1_TRIGGER_ARRAY.size(); b++){
-      TString label="";
-      label = "L1_"; label+=L1_TRIGGER_ARRAY.at(b);
-      triggers_h[i]->GetXaxis()->SetBinLabel(b+1,label.Data());
+  for (short unsigned int i = 0; i < n_each_h1D; i++) {
+    title1 = "triggers_h["; title1 += i; title1 += "]";
+    title2 = title1; title2 += "; trigger xxx";
+    triggers_h[i] = new TH1F(title1.Data(), title2.Data(), 12, 0, 12);
+    for (short unsigned int b = 1; b < L1_TRIGGER_ARRAY.size(); b++) {
+      TString label = "";
+      label = "L1_"; label += L1_TRIGGER_ARRAY.at(b);
+      triggers_h[i]->GetXaxis()->SetBinLabel(b + 1, label.Data());
     };
-    triggers_h[i]->SetDirectory(directory);     
+    triggers_h[i]->SetDirectory(directory);
   };
   h1D->push_back(triggers_h);
 
 
-  for(short unsigned int i=0;i<n_each_h2D; i++){  
-    title1 = "run_vs_bx_h["; title1+=(i); title1+="]";
-    title2 = title1; title2+=";BCN";
-    run_vs_bx_h[i] = new TH2F(title1.Data(),title2.Data(), 3100, -1,3099, 1100, 210490, 211590);
-    run_vs_bx_h[i]->SetDirectory(directory); 
+  for (short unsigned int i = 0; i < n_each_h2D; i++) {
+    title1 = "run_vs_bx_h["; title1 += (i); title1 += "]";
+    title2 = title1; title2 += ";BCN";
+    run_vs_bx_h[i] = new TH2F(title1.Data(), title2.Data(), 3100, -1, 3099, 1100, 210490, 211590);
+    run_vs_bx_h[i]->SetDirectory(directory);
 
-    title1 = "run_vs_ls_h["; title1+=(i); title1+="]";
-    title2 = title1; title2+=";LS";
-    run_vs_ls_h[i] = new TH2F(title1.Data(),title2.Data(), 1000, -2, 1998, 1100, 210490, 211590);
-    run_vs_ls_h[i]->SetDirectory(directory); 
+    title1 = "run_vs_ls_h["; title1 += (i); title1 += "]";
+    title2 = title1; title2 += ";LS";
+    run_vs_ls_h[i] = new TH2F(title1.Data(), title2.Data(), 1000, -2, 1998, 1100, 210490, 211590);
+    run_vs_ls_h[i]->SetDirectory(directory);
   };
   h2D->push_back(run_vs_bx_h);
   h2D->push_back(run_vs_ls_h);
--- src/uacmsevtinfo.h
+++ src/uacmsevtinfo.h
@@ -21,42 +21,41 @@ const std::vector<unsigned int>L1_TRIGGER_ARRAY = {
 };
 
 // const unsigned int ALGO_TRIGGER_ARRAY[3] = {99,  // L1_CastorEm_TotemLowMultiplicity
-// 					    105, // L1_DoubleJet20_TotemDiffractive
-// 					    122};// L1_HcalHfSingleChannel_BptxAND_Instance1 (passive)
-// const unsigned int TT_TRIGGER_ARRAY[5]   ={ 0, //	L1Tech_BPTX_plus_AND_minus.v0 (passive) //Run2: 10 && 11 or Or anything else
-// 					    7, //	L1Tech_BPTX_quiet.v0 (passive) // Run2: 9
-// 					    9, // 	L1Tech_HCAL_HF_coincidence_PM.v2 (passive)
-// 					    52, // L1Tech_TOTEM_Diffractive.v0 (passive)
-// 					    53}; 	// L1Tech_TOTEM_MinBias.v0
-class uacmsevtinfo :  public uabase
-{
+//              105, // L1_DoubleJet20_TotemDiffractive
+//              122};// L1_HcalHfSingleChannel_BptxAND_Instance1 (passive)
+// const unsigned int TT_TRIGGER_ARRAY[5]   ={ 0, //  L1Tech_BPTX_plus_AND_minus.v0 (passive) //Run2: 10 && 11 or Or anything else
+//              7, // L1Tech_BPTX_quiet.v0 (passive) // Run2: 9
+//              9, //   L1Tech_HCAL_HF_coincidence_PM.v2 (passive)
+//              52, // L1Tech_TOTEM_Diffractive.v0 (passive)
+//              53};  // L1Tech_TOTEM_MinBias.v0
+class uacmsevtinfo :  public uabase {
 public:
-    uacmsevtinfo( TChain     * tree, 	 //!<tree of ua format
-		  TDirectory * dir,    //!<directory in the output root file
-		  const bool cmstotem, //!<true for merged, false for CMS only
-		  const short          int MC,  //!< -1, 0 - data; >0 MC
-		  const short unsigned int Ncuts //!< number of cuts);
-		  );
-    ~uacmsevtinfo();
-    bool GetL1Bit(short unsigned int b){return CMStrigInfo->triggerResults.at(b);};
-    bool CheckHLT(const char * path);
-    void PrintEventInfo(const bool detailed);
-    bool FillLastEvent(const short unsigned int cut);
-    bool ProceedEvent(const short unsigned int cut, const bool fill, const bool info);
+  uacmsevtinfo(TChain      *tree,     //!<tree of ua format
+               TDirectory *dir,     //!<directory in the output root file
+               const bool cmstotem, //!<true for merged, false for CMS only
+               const short          int MC,  //!< -1, 0 - data; >0 MC
+               const short unsigned int Ncuts //!< number of cuts);
+              );
+  ~uacmsevtinfo();
+  bool GetL1Bit(short unsigned int b) {return CMStrigInfo->triggerResults.at(b);};
+  bool CheckHLT(const char *path);
+  void PrintEventInfo(const bool detailed = false);
+  bool FillLastEvent(const short unsigned int cut);
+  bool ProceedEvent(const short unsigned int cut, const bool fill, const bool info);
 
 private:
-    void create_histos();
+  void create_histos();
 
-    MyEvtId*      CMSevtInfo;
-    MyL1TrigRun2* CMStrigInfo;
-    MyL1MenuRun2* CMSmenuInfo;
-    MyHLTrig*     CMSHLT;
+  MyEvtId      *CMSevtInfo;
+  MyL1TrigRun2 *CMStrigInfo;
+  MyL1MenuRun2 *CMSmenuInfo;
+  MyHLTrig     *CMSHLT;
 
-    TH1F ** triggers_h;
-    TH2F ** run_vs_bx_h;
-    TH2F ** run_vs_ls_h;
+  TH1F **triggers_h;
+  TH2F **run_vs_bx_h;
+  TH2F **run_vs_ls_h;
 
-    ClassDef(uacmsevtinfo,3);
+  ClassDef(uacmsevtinfo, 3);
 };
 
 #endif // UACMSEVTINFO_H
