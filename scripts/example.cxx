1042f16b0713c4e2854679b24dd3f6f01a181d5e
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2017-08-25 16:14:10 +0200

    Added directory for scripts and added example ROOT script

---
+++ scripts/example.cxx
@@ -0,0 +1,6 @@
+{
+uaplotter1 *p = new uaplotter1(0,0,1,-3);
+p->Loop(-1,-1);
+
+delete p;
+}
--- scripts/start.cxx
+++ scripts/start.cxx
@@ -1,4 +1,6 @@
 { 
+gROOT->ProcessLine(".include ../UATree/UADataFormat/interface");
+gROOT->ProcessLine(".include ../TOTEMdataFormat/src");
 gROOT->ProcessLine(".L ../UATree/UADataFormat/lib/libUADataFormat.so");
 gROOT->ProcessLine(".L ../TOTEMdataFormat/lib/libTOTEMdataFormat.so");
 gROOT->ProcessLine(".L lib/libuaplotter.so");
