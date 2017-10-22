90ca9c620cf22056799061eb428045a042254792
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2017-10-22 05:30:40 +0300

    Updated script start.cxx

--- scripts/start.cxx
+++ scripts/start.cxx
@@ -1,6 +1,7 @@
-{ 
+{
 gROOT->ProcessLine(".include ../UATree/UADataFormat/interface");
-gROOT->ProcessLine(".include ../TOTEMdataFormat/src");
+gROOT->ProcessLine(".include ../TOTEMdataFormat/interface");
+gROOT->ProcessLine(".include ../interface");
 gROOT->ProcessLine(".L ../UATree/UADataFormat/lib/libUADataFormat.so");
 gROOT->ProcessLine(".L ../TOTEMdataFormat/lib/libTOTEMdataFormat.so");
 gROOT->ProcessLine(".L lib/libuaplotter.so");
