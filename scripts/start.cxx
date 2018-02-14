57f3975bc9bcb1ce1776ddf8a7c5af1450efe4b1
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2018-02-15 01:18:22 +0300

    start.cxx updated to be more generalized

--- scripts/start.cxx
+++ scripts/start.cxx
@@ -1,8 +1,13 @@
 {
-gROOT->ProcessLine(".include ../UATree/UADataFormat/interface");
-gROOT->ProcessLine(".include ../TOTEMdataFormat/interface");
-gROOT->ProcessLine(".include ../interface");
-gROOT->ProcessLine(".L ../UATree/UADataFormat/lib/libUADataFormat.so");
-gROOT->ProcessLine(".L ../TOTEMdataFormat/lib/libTOTEMdataFormat.so");
-gROOT->ProcessLine(".L lib/libuaplotter.so");
+string s_include_dir = string(".include ");
+string s_library_dir = string(".L ");
+string cmssw_base = gSystem->Getenv("CMSSW_BASE");
+
+gROOT->ProcessLine( (s_include_dir+cmssw_base+string("/src/uaplotter1/interface")).c_str() );
+gROOT->ProcessLine( (s_include_dir+cmssw_base+string("/src/UATree/UADataFormat/interface")).c_str() );
+gROOT->ProcessLine( (s_include_dir+cmssw_base+string("/src/TOTEMdataFormat/interface")).c_str() );
+
+gROOT->ProcessLine((s_library_dir+cmssw_base+string("/src/UATree/UADataFormat/lib/libUADataFormat.so")).c_str());
+gROOT->ProcessLine((s_library_dir+cmssw_base+string("/src/TOTEMdataFormat/lib/libTOTEMdataFormat.so")).c_str());
+gROOT->ProcessLine((s_library_dir+cmssw_base+string("/src/uaplotter1/lib/libuaplotter.so")).c_str());
 }
