6071b0292829474df9f47987fc010020dfa9a63b
Dmitry Sosnov <dmitry.sosnov@cern.ch>
2018-02-26 23:24:40 +0300

    Added QGSJet-II

--- plots/2016/plotnRGs.cxx
+++ plots/2016/plotnRGs.cxx
@@ -11,15 +11,19 @@ void plotnRGs()
   auto data_pbp = TFile::Open("uaplot_output_histos_data_PARun2016_Pbp.root","r");
   auto hijing_ppb = TFile::Open("uaplot_output_histos_Hijing_pPb.root","r");
   auto hijing_pbp = TFile::Open("uaplot_output_histos_Hijing_Pbp.root","r");
+  auto qgsjet_ppb = TFile::Open("uaplot_output_histos_QGSJET_pPb.root","r");
+  auto qgsjet_pbp = TFile::Open("uaplot_output_histos_QGSJET_Pbp.root","r");
 
   std::vector<std::tuple<TFile*, std::string, int, int, int>> roots_ppb = {
     std::make_tuple(epos_ppb,  "EPOS, pPb", 3, 3006, 22),
     std::make_tuple(hijing_ppb,"Hijing, pPb", 4, 3007, 23),
+    std::make_tuple(qgsjet_ppb,  "QGSJet-II, pPb", 8, 3006, 33),
     std::make_tuple(data_ppb,  "Data, pPb", 2, 3004, 20)
   };
   std::vector<std::tuple<TFile*, std::string, int, int, int>> roots_pbp = {
     std::make_tuple(epos_pbp,  "EPOS, Pbp", 3, 3006, 22),
     std::make_tuple(hijing_pbp,"Hijing, Pbp", 4, 3007, 23),
+    std::make_tuple(qgsjet_pbp,  "QGSJet-II, Pbp", 8, 3006, 33),
     std::make_tuple(data_pbp,  "Data, Pbp", 2, 3004, 20)
   };
   std::vector<std::tuple< std::vector<std::tuple<TFile*, std::string, int, int, int>>, std::string  >> roots = {
@@ -27,9 +31,10 @@ void plotnRGs()
     std::make_tuple(roots_pbp, "pbp")
   };
 
-  if(!true){ // Empty bins distribituon
+  if(true){ // Empty bins distribituon
     vector<tuple<string, string>> histograms = {
       make_tuple("CMSpf/activity_loose_h", "pf_emptyBins_loose"),
+      make_tuple("CMSpf/activity_tight_h", "pf_emptyBins_tight"),
       make_tuple("CMStracking/activity_tight_h", "tracking_emptyBins_tight"),
       make_tuple("CMStracking/activity_loose_h", "tracking_emptyBins_loose"),
       make_tuple("central_activity_h", "emptyBins")
@@ -71,7 +76,7 @@ void plotnRGs()
       }
     }
   }
-  if(!true){ // Distribution of \Delta_{\eta^F} minus/plus.
+  if(true){ // Distribution of \Delta_{\eta^F} minus/plus.
     for(auto rr: roots){
       std::vector<std::tuple<std::string, std::string, int>> histograms = {
         std::make_tuple("n_sd_minus_bins_h", "minusBins", 10),
@@ -126,7 +131,7 @@ void plotnRGs()
       }
     }
   }
-  if(!true){ // Distribution of \Delta_{\eta^F} minus/plus per PID. //TODO
+  if(true){ // Distribution of \Delta_{\eta^F} minus/plus per PID. //TODO
     std::vector<std::tuple<std::string, std::string, int>> histograms = {
       std::make_tuple("n_sd_minus_bins_h", "minusBins_byPids", 25), //25..29 is "on right side > 4"
       std::make_tuple("n_sd_plus_bins_h",  "plusBins_byPids" , 20)  //20..25 is "on left side > 4"
@@ -149,6 +154,91 @@ void plotnRGs()
     std::vector<std::tuple< TFile*, std::string>> files_with_pids = {
       std::make_tuple(epos_ppb, "epos_ppb"),
       std::make_tuple(epos_pbp, "epos_pbp")
+    };
+    for(auto r: files_with_pids){
+      for(auto h: histograms){
+        string outFile = std::get<1>(r) + string("_") + std::get<1>(h);
+        auto *c = new TCanvas(outFile.c_str(), "", 600, 600);
+        auto th = new THStack(std::get<1>(h).c_str(), "; |#Delta_{#eta^F}|");
+        auto leg = new TLegend(0.69, 0.69, 0.99, 0.92);
+        auto countOfEvents = ((TH1F*)(std::get<0>(r))->Get("CMScalo/hf_etotal_minus_h[0]"))->GetEntries();
+        auto currentHistN = 0;
+        for (int i = 0; i < pids.size(); i++){
+          TH1F* h0;
+          for(auto j = 0; j < std::get<3>(pids.at(i)); j++){
+            auto histname = std::get<0>(h) + string("[") + to_string(std::get<2>(h)+currentHistN) + string("]");
+          COUT_VALUE(outFile);
+          COUT_VALUE(histname);
+            currentHistN++;
+            auto h00  = (TH1F*)((std::get<0>(r))->Get(histname.c_str())->Clone());
+            if(j==0){
+              h0 = h00;
+            }else{
+              h0->Add(h00);
+            }
+          }
+          // auto h0 = (TH1F*)((std::get<0>(r))->Get(histname.c_str())->Clone());
+          auto hist = new TH1F(std::get<1>(r).c_str(),std::get<1>(r).c_str(),13, 0, 6.50);
+          // h0->Sumw2(); h0->Scale(1.0/h0->GetEntries());
+          h0->Sumw2();
+          for(int i = 2; i < 15; i++){ //Setting x axis to eta range
+            hist->SetBinContent(i-1, h0->GetBinContent(i));
+            hist->SetBinError(i-1, h0->GetBinError(i));
+            // cout << "hist " << i << " " << hist->GetBinContent(i-1) << " " << hist->GetBinError(i-1) << endl;
+          }
+          hist->Print();
+          hist->SetTitle(std::get<0>(pids.at(i)).c_str());
+          // hist->SetTitle(std::get<2>(pids.at(i)).c_str());
+          hist->SetLineColor(std::get<1>(pids.at(i)));
+          hist->SetLineWidth(3);
+          hist->SetMarkerColor(std::get<1>(pids.at(i)));
+          // hist->SetMarkerStyle(std::get<4>(pids.at(i)));
+          // hist->SetFillColor(std::get<1>(pids.at(i)));
+          // hist->SetFillStyle(3004);
+          // hist->Sumw2(); hist->Scale(1.0/countOfEvents);
+          hist->Scale(1.0/countOfEvents);
+          // for(int i = 1; i <= hist->GetNbinsX(); i++){ //Setting x axis to eta range
+          //  hist->GetXaxis()->SetBinLabel(i, ((i%2)?string(""):to_string(i/2-1)).c_str());
+          // }
+          hist->Print();
+          th->Add(hist); leg->AddEntry(hist);
+        }
+        th->Draw("nostack,e");
+        leg->Draw();
+        th->SetMinimum(1E-6);
+        th->SetMaximum(1);
+        c->SetLogy();
+        c->SaveAs((outFile+string("_log")+string(".eps")).c_str());
+        c->SetLogy(false);
+        th->SetMaximum(0.3);
+        c->SaveAs((outFile+string("_lin")+string(".eps")).c_str());
+        c->Clear();
+      }
+    }
+  }
+  if(true){ // Distribution of \Delta_{\eta^F} minus/plus per PID. -- QGSJet //TODO
+    std::vector<std::tuple<std::string, std::string, int>> histograms = {
+      std::make_tuple("n_sd_minus_bins_h", "minusBins_byPids", 25), //25..29 is "on right side > 4"
+      std::make_tuple("n_sd_plus_bins_h",  "plusBins_byPids" , 20)  //20..25 is "on left side > 4"
+    };
+    std::vector<std::tuple<std::string, int, std::string, int>> pids = {
+      std::make_tuple("ND",         3, "ND", 1),
+      // std::make_tuple("CD",         7, "ND && CD", 1),
+      // std::make_tuple("SD-[RG@-Z]", 4, "ND && CD && SD-", 1),
+      // std::make_tuple("SD+[RG@+Z]", 2, "ND && CD && SD- && SD+", 1),
+      std::make_tuple("SD & CD",   4, "CD && SD- && SD+", 3),
+      std::make_tuple("DD",         6, "ND && CD && SD- && SD+ && DD", 1)
+    };
+    // std::vector<std::tuple<std::string, int, std::string>> pids = {
+    //   std::make_tuple("ND",         3, "ND"),
+    //   std::make_tuple("CD",         6, "ND && CD"),
+    //   std::make_tuple("SD-[RG@-Z]", 4, "ND && CD && SD-"),
+    //   std::make_tuple("SD+[RG@+Z]", 2, "ND && CD && SD- && SD+"),
+    //   std::make_tuple("DD",         7, "ND && CD && SD- && SD+ && DD")
+    // };
+    std::vector<std::tuple< TFile*, std::string>> files_with_pids = {
+      std::make_tuple(qgsjet_ppb, "qgsjet_ppb"),
+      std::make_tuple(qgsjet_pbp, "qgsjet_pbp")
     };
     for(auto r: files_with_pids){
       for(auto h: histograms){
@@ -285,7 +375,7 @@ void plotnRGs()
       }
     }
   }
-  if(!true){ // PIDs
+  if(true){ // PIDs
     for(auto rr: roots){
       string outFile = string("pids_") + std::get<1>(rr);
       cout << "outFile: " << outFile << endl;
